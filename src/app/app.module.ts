a7ddbc048d7e60bd94c586e495d5a89af9764433
Alexandre Desroches <alex.desroches7@gmail.com>
2022-09-05 17:30:25 -0400

    testing Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CalcComponent } from './components/calc/calc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalcComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/calc/calc.component.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calc',
+  templateUrl: './calc.component.html',
+  styleUrls: ['./calc.component.css']
+})
+export class CalcComponent implements OnInit {
+  constructor() { }
+
+  public rounded = {
+    subTotal: 0,
+    tps: 0,
+    tvq: 0,
+    total: 0,
+  }
+
+  private raw = {
+    subTotal: 0,
+    tps: 0,
+    tvq: 0,
+    total: 0,
+  }
+
+  private taxRates = {
+    quebec: {
+      tps: 0.05,
+      tvq: 0.09975,
+    }
+  }
+
+  ngOnInit(): void {
+    const autofocusInput = document.getElementById("subTotal") as HTMLInputElement
+    if (autofocusInput) {
+      autofocusInput.focus()
+    }
+  }
+
+  getInputNumber(event: Event) {
+    const target = event.target as HTMLInputElement
+    const value = target.value
+    const number = Number(this.replaceCommas(value))
+    return isNaN(number) ? 0 : number
+  }
+
+  replaceCommas(value: string) {
+    return value?.replace(",", ".")
+  }
+
+  updateSubtotal(event: Event) {
+    this.raw.subTotal = this.getInputNumber(event)
+    this.calculateTotal()
+  }
+
+  updateTotal(event: Event) {
+    this.raw.total = this.getInputNumber(event)
+    this.calculateSubTotal()
+  }
+
+  roundNumber(num: number, decimals = 2) {
+    return Math.round((Number(num) + Number.EPSILON) * 100) / 100
+  }
+
+  calculateTotal() {
+    this.raw.tps = this.raw.subTotal * this.taxRates.quebec.tps
+    this.raw.tvq = this.raw.subTotal * this.taxRates.quebec.tvq
+    this.raw.total = this.raw.subTotal + this.raw.tps + this.raw.tvq
+    this.roundValues()
+  }
+
+  calculateSubTotal() {
+    this.raw.subTotal = this.raw.total / (1 + this.taxRates.quebec.tps + this.taxRates.quebec.tvq)
+    this.raw.tps = this.raw.subTotal * this.taxRates.quebec.tps
+    this.raw.tvq = this.raw.subTotal * this.taxRates.quebec.tvq
+    this.roundValues()
+  }
+
+  roundValues() {
+    this.rounded.subTotal = this.roundNumber(this.raw.subTotal);
+    this.rounded.tps = this.roundNumber(this.raw.tps);
+    this.rounded.tvq = this.roundNumber(this.raw.tvq);
+    this.rounded.total = this.roundNumber(this.raw.total);
+  }
+
+}
